3d70f11beef36d69507035ddf432bcf959dd4233
legitrue <channborry@gmail.com>
2024-11-12 20:46:07 +0300

    Header

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,17 @@
 import { Header } from "./components/Header";
+<<<<<<< HEAD
 import { BlogPage } from "./page/BlogPage";
+=======
+>>>>>>> f755740e6654d64cf6a5cc9a973a9ce64fdf3102
 
 function App() {
   return <>
   
   <Header/>
+<<<<<<< HEAD
   <BlogPage/>
+=======
+>>>>>>> f755740e6654d64cf6a5cc9a973a9ce64fdf3102
   </>;
  
 }
