8236e54b0d28c34fe304bcaea96e60c449f313b8
serewe <serewe6l@gmail.com>
2024-11-15 20:39:37 +0300

    fix header

---
+++ client/src/pages/HomePage.tsx
@@ -0,0 +1,21 @@
+import { FC } from 'react';
+import { Header } from '../components/Header';
+
+const HomePage: FC = () => {
+  return (
+    <>
+      <Header />
+
+      <div>
+        <div>
+          <h1 className="text-9xl">
+            Ve<span className="">Q</span>tra
+          </h1>
+        </div>
+        <div></div>
+      </div>
+    </>
+  );
+};
+
+export default HomePage;
